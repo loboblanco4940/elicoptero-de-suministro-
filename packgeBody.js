fa4d837b7c082b0168de3b27289c4caffcecd197
loboblanco4940 <92131004+loboblanco4940@users.noreply.github.com>
2021-10-27 21:16:29 -0500

    Add files via upload

---
+++ packgeBody.js
@@ -0,0 +1,14 @@
+class packgeBody {
+    constructor(x, y, width, height) {
+      var options = {
+          'restitution':0.8,
+          
+      }
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      
+      World.add(world, this.body);
+    }
+    
+  };
